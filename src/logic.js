efcd6dd0743a260b814736229b3a62bfef9f84e0
Pete Fowler <fowler.pete@gmail.com>
2022-07-05 21:15:43 -0600

    Process current weather object
    Need to rewrite function names to indicate this is currentweather, add
    forecast

--- src/logic.js
+++ src/logic.js
@@ -1,9 +1,17 @@
+const weather = {}
 
+const processWeather = (object) => {
+  weather.main = object.main;
+  weather.description = object.weather[0].description;
+  weather.wind = object.wind;
+  console.log(object);
+  console.log(weather);
+}
 
 const getWeather = (location) => {
-  console.log('a');
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=1d251f3ea86291b4dd946b1949e21ad8&units=imperial`, {mode: 'cors'})
-  .then((data) => {console.log(data.json())})
+  .then((data) => data.json())
+  .then((data) => processWeather(data))
   .catch(err => console.log(err));
 }
 
