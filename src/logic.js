eab1db88e2e572e5e83b763654ace8204f20f132
Pete Fowler <fowler.pete@gmail.com>
2022-07-06 06:54:56 -0600

    Get forecast object
    Consider switching to NWS.gov api. Openweather doesn't allow free
    access to daily forecasts, only hourly versions

--- src/logic.js
+++ src/logic.js
@@ -1,11 +1,19 @@
 const weather = {}
 
 const processWeather = (object) => {
-  weather.main = object.main;
-  weather.description = object.weather[0].description;
-  weather.wind = object.wind;
+  weather.current = {
+    main: object.main,
+    description: object.weather[0].description,
+    wind: object.wind.speed,
+  }
+  // console.log(object);
+  // console.log(weather);
+  }
+  
+
+
+const processForecast = (object) => {
   console.log(object);
-  console.log(weather);
 }
 
 const getWeather = (location) => {
@@ -15,4 +23,12 @@ const getWeather = (location) => {
   .catch(err => console.log(err));
 }
 
+const getForecast = (location) => {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=1d251f3ea86291b4dd946b1949e21ad8&units=imperial`, {mode: 'cors'})
+    .then((data) => data.json())
+    .then((data) => processForecast(data))
+    .catch(err => console.log(err));
+}
+
 getWeather('Denver');
+getForecast('Denver');
