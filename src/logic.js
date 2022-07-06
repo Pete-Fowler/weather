5ef63ad9fcb1f5dd93f1a6e073854cd0a1e9ba45
Pete Fowler <fowler.pete@gmail.com>
2022-07-05 19:55:54 -0600

    Obtain weather promise object

--- src/logic.js
+++ src/logic.js
@@ -1,8 +1,10 @@
 
 
 const getWeather = (location) => {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=1d251f3ea86291b4dd946b1949e21ad8`, {mode: 'cors'})
-  .then((data) => console.log(data));
+  console.log('a');
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=1d251f3ea86291b4dd946b1949e21ad8&units=imperial`, {mode: 'cors'})
+  .then((data) => {console.log(data.json())})
+  .catch(err => console.log(err));
 }
 
 getWeather('Denver');
