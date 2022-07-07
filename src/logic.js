38fabb8e3b9a273496d8b46c7d5feb3f9021ebde
Pete Fowler <fowler.pete@gmail.com>
2022-07-06 19:15:35 -0600

    getLatLon() complete

--- src/logic.js
+++ src/logic.js
@@ -1,5 +1,6 @@
 const weather = {}
 
+// needs updating
 const processWeather = (object) => {
   weather.current = {
     main: object.main,
@@ -9,20 +10,19 @@ const processWeather = (object) => {
   // console.log(object);
   // console.log(weather);
   }
-  
-
 
 const processForecast = (object) => {
   console.log(object);
 }
 
-const getCoordinates = (search) => {
-  fetch(`http://dev.virtualearth.net/REST/v1/Locations?query=${search}&key=AiOvouOlhdmW8YH-PSBnwW4MHdU654a580GdTnm2UJjFLjFFNaVGWndcsoZIC1r8`, {mode: 'cors'})
-  .then(data => data.json())
-  .then(data => data.resourceSets[0].resources[0].point.coordinates)
-  .then(data => console.log(`${data[0]}, ${data[1]}`));
+const getLatLon = async (search) => {
+  const response = await fetch(`http://dev.virtualearth.net/REST/v1/Locations?query=${search}&key=AiOvouOlhdmW8YH-PSBnwW4MHdU654a580GdTnm2UJjFLjFFNaVGWndcsoZIC1r8`, {mode: 'cors'});
+  const data = await response.json();
+  const coordinates = data.resourceSets[0].resources[0].point.coordinates;
+  return coordinates;
 }
 
+// switch to NWS
 const getWeather = (location) => {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=1d251f3ea86291b4dd946b1949e21ad8&units=imperial`, {mode: 'cors'})
   .then((data) => data.json())
@@ -30,20 +30,22 @@ const getWeather = (location) => {
   .catch(err => console.log(err));
 }
 
-const getForecast = (lat, lon) => {
-    // fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=1d251f3ea86291b4dd946b1949e21ad8&cnt=5&units=imperial`, {mode: 'cors'})
-    fetch(`https://api.weather.gov/points/${lat},${lon}`, { mode: 'cors'})
-    .then((data) => data.json())
-    .then((data) => fetch(data.properties.forecast))
-    .then((data) => data.json())
-    .then((data) => processForecast(data))
-    .catch(err => console.log(err));
+const getForecast = async (coordinates) => {
+    const response = await fetch(`https://api.weather.gov/points/${coordinates[0]},${coordinates[1]}`, { mode: 'cors'})
+    const data = await response.json();
+    const forecast = await fetch(data.properties.forecast);
+    const forecastJSON = await forecast.json();
+    processForecast(forecastJSON);
 }
 
 
 // Fwd search bing maps
 // http://dev.virtualearth.net/REST/v1/Locations?query={locationQuery}&key={AiOvouOlhdmW8YH-PSBnwW4MHdU654a580GdTnm2UJjFLjFFNaVGWndcsoZIC1r8}
 
-getCoordinates('Denver, CO');
+// Code shows how to use getCoordinates()
+getLatLon('Denver, CO').then(data => console.log(data));
+
+
+// code for testing
 // getWeather('Denver');
-// getForecast(39.684765, -105.129866);
+// getForecast([39.684765, -105.129866]);
